ae8f147dafbc4b6dadfb56c6227c92cd781378b9
paniklas <paniklas@hotmail.com>
2024-10-27 09:05:52 +0200

    setup projext and create home page

---
+++ app/(root)/page.tsx
@@ -0,0 +1,53 @@
+import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
+
+export default async function Home({ searchParams }: { searchParams: Promise<{ query?: string }> }) {
+
+  const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "Panos" },
+      _id: 1,
+      description: "This is a description",
+      image: "https://images.unsplash.com/photo-1532178324009-6b6adeca1741?q=80&w=5184&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      category: "Robots",
+      title: "We Robots"
+    }
+  ];
+
+  return (
+      <>
+        <section className="pink_container">
+          <h1 className="heading">Pitch Your Startup, <br /> Connect with Entrepreneurs</h1>
+
+          <p className="sub-heading !max-w-3xl">
+            Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
+          </p>
+
+          <SearchForm query={query} />
+        </section>
+
+        <section>
+          <div className="section_container">
+            <p className="text-30-semibold">
+              {query ? `Search results for "${query}"` :  "All Startups" }
+            </p>
+
+            <ul className="mt-7 card_grid">
+              {posts?.length > 0 ? (
+                posts.map((post: StartupCardType ) => (
+                  <StartupCard key={post?._id} post={post} />
+                ))
+              ) : (
+                <p className="no-results">No startups found</p>
+            )}
+            </ul>
+
+          </div>
+        </section>
+      </>
+  );
+}
